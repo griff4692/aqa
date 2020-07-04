bdefedb8f336a223dc03b35cfc35dbe6ecc20bb7
Griffin Adams <griff4692@gmail.com>
2020-07-04 18:23:47 -0400

    more

--- data/main.py
+++ data/main.py
@@ -1,31 +1,30 @@
 from functools import partial
-import json
 from multiprocessing import Pool, Manager
 import os
+import pickle
 import sys
 
-import neuralcoref
+from allennlp.predictors.predictor import Predictor
 from nlp import load_dataset
-import spacy
 from torch.utils.data import Subset
 
 
-def process_corref(example, ctr=None, lock=None):
-    contexts = example['entity_pages']
-    docs = list(map(lambda d: spacy_nlp(d).to_json(), contexts['wiki_context']))
-    if len(docs) > 0:
-        print(docs[0])
+def construct_graph(example, ctr=None, lock=None):
+    contexts = list(map(lambda x: {'sentence': x}, example['entity_pages']['wiki_context']))
+    coref_pred = predictor.predict_batch_json(contexts)
     with lock:
         ctr.value += 1
         if ctr.value % 1 == 0:
             print('Processed {} examples'.format(ctr.value))
+
+    print(coref_pred)
 
     # Include question_id and doc titles just to make sure we serialize in same order (as FK into full dataset)
     return {
         'question_id': example['question_id'],
         'entity_pages': {
             'title': contexts['title'],
-            'spacy_doc': docs
+            'coref': coref_pred
         }
     }
 
@@ -36,27 +35,25 @@ if __name__ == '__main__':
     print('Loading Dataset')
     dataset = load_dataset('trivia_qa', 'rc')
     print('Loading Coref Pipeline')
-    spacy_nlp = spacy.load('en_core_web_lg')
-    coref = neuralcoref.NeuralCoref(spacy_nlp.vocab)
-    spacy_nlp.add_pipe(coref, name='neuralcoref')
+    predictor = Predictor.from_path(
+        'https://storage.googleapis.com/allennlp-public-models/openie-model.2020.03.26.tar.gz')
 
     dtypes = list(dataset.keys())  # train, test, etc,
     for dtype in dtypes:
         d = dataset[dtype]
         if debug_mode:
-            d = Subset(d, list(range(2)))
+            d = Subset(d, list(range(1, 3)))
         n = len(d)
         print('Processing {} examples from {} set'.format(n, dtype))
-        out_fn = os.path.join('trivia_qa', 'coref_{}.json'.format(dtype))
+        out_fn = os.path.join('trivia_qa', 'coref_{}.pk'.format(dtype))
         with Manager() as manager:
             p = Pool()
             lock = manager.Lock()
             ctr = manager.Value('i', 0)
-            clusters = list(p.map(partial(process_corref, ctr=ctr, lock=lock), d))
+            clusters = list(map(partial(construct_graph, ctr=ctr, lock=lock), d))
             qids = set(list(map(lambda c: c['question_id'], clusters)))
         print('Unique QIDs={}'.format(len(qids)))
         print(len(clusters))
-
-        with open(out_fn, 'w') as fd:
-            json.dump(clusters, fd)
+        with open(out_fn, 'wb') as fd:
+            pickle.dump(clusters, fd)
         print('Saved pickled output to {}'.format(out_fn))
