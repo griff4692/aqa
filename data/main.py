73d6d8cb56191aced2ea62742eb78c3bf2edd903
Griffin Adams <griff4692@gmail.com>
2020-07-06 18:33:23 -0400

    Remove weights.

--- data/main.py
+++ data/main.py
@@ -1,10 +1,14 @@
 from collections import defaultdict
 import os
+import json
 import pickle
 import re
 import sys
 
 from allennlp.predictors.predictor import Predictor
+from allennlp.models.archival import load_archive
+from allennlp.common.util import import_submodules, sanitize
+import_submodules('imojie')
 import matplotlib.pyplot as plt
 import networkx as nx
 import neuralcoref
@@ -13,6 +17,7 @@ import spacy
 from torch.utils.data import Subset
 
 ARG_REGEX = r'\[ARG(\d{1,3}: [^\[\]]+)\]'
+MODIFIER_REGEX = r'\[ARGM-[A-Z]+: [^\[\]]+)\]'
 SIM_THRESHOLD = 0.7
 
 
@@ -23,7 +28,7 @@ def _node_match(new_node, graph, alt_names):
         full_comp = [node] + list(alt_names.get(new_node, []))
         max_overlap = max(list(map(lambda x: overlap(new_node, x), full_comp)))
         if max_overlap >= max(max_sim, SIM_THRESHOLD):
-            print('Merging {} into {}'.format(new_node, node))
+            print('Merging \'{}\' --> \'{}\''.format(new_node, node))
             max_sim = max_overlap
             match = node
     return match
@@ -35,17 +40,19 @@ def _sentence_split(str):
 
 def extract_args(description):
     arg_strs = re.findall(ARG_REGEX, description)
-    results = []
-    prev_d = -1
+    results = defaultdict(list)
+    modifiers = {}
     for arg_str in arg_strs:
         d, arg = arg_str.split(':')
         d = int(d)
-        if not d > prev_d:
-            raise Exception(description)
         arg = arg.strip()
-        prev_d = d
-        results.append(arg.lower())
-    return list(set(results))
+        results[d].append(arg.lower())
+
+    modifier_strs = re.findall(MODIFIER_REGEX, description)
+    for mod_str in modifier_strs:
+        mod, text = mod_str.split(':')
+        modifiers[mod.strip()] = text.strip().lower()
+    return results, modifiers
 
 
 def handle_node(node, graph, alt_names, node_weights):
@@ -64,11 +71,52 @@ def overlap(a, b):
     return (feats[0, :] * feats[1, :].T).todense()[0, 0]
 
 
+def process_imojie_output(token_ids):
+    temp=" ".join(token_ids)
+    temp = temp.replace(" ##","")
+    temp = temp.replace("[unused1]","( ")
+    temp = temp.replace("[unused2]"," ; ")
+    temp = temp.replace("[unused3]","")
+    temp = temp.replace("[unused4]"," ; ")
+    temp = temp.replace("[unused5]","")
+    temp = temp.replace("[unused6]"," )")
+    temp = temp.strip()
+    temp = temp.split('[SEP]')
+    ans = []
+    for x in temp:
+        if x != '':
+            ans.append(x)
+    return ans
+
+
+def extract_tups_from_str(str):
+    return list(map(lambda x: x.strip().lower(), str.strip('() ').split(';')))
+
+
+def _construct_ie_tuples(args, idx):
+    if idx not in args:
+        return [[]]
+
+    results = []
+    next_paths = _construct_ie_tuples(args, idx + 1)
+    for arg in args[idx]:
+        for path in next_paths:
+            results.append([arg] + path)
+
+    return results
+
+
+def construct_ie_tuples(args, verb):
+    all_paths = _construct_ie_tuples(args, 0)
+    all_paths_w_verb = map(lambda x: x[0] + [verb] + x[1:], all_paths)
+    return all_paths_w_verb
+
+
 def construct_graph(example, verbose=True):
     contexts = example['entity_pages']
     sents = []
     clusters = defaultdict(set)
-    coref_contexts =  []
+    coref_contexts = []
     for context in contexts['wiki_context']:
         coref = coref_nlp(context)._
         coref_contexts.append(coref.coref_resolved)
@@ -78,34 +126,38 @@ def construct_graph(example, verbose=True):
             mentions = list(map(lambda x: x.text.lower(), cluster.mentions))
             clusters[cluster.main.text.lower()].update(mentions)
 
-    batch_sents = list(map(lambda x: {'sentence': x}, sents))
-    ie_outputs = predictor.predict_batch_json(batch_sents)
+    ie_tups = []
+    for sent in sents:
+        inp_instance = predictor._dataset_reader.text_to_instance(sent)
+        output = predictor._model.forward_on_instance(inp_instance)
+        output = sanitize(output)
+        output = process_imojie_output(output["predicted_tokens"][0])
+        ie_tup = list(map(extract_tups_from_str, output))
+        ie_tup_valid = list(filter(lambda x: len(x) == 3, ie_tup))
+
+        if len(ie_tup_valid) < len(ie_tup):
+            invalid_tups = list(set(ie_tup) - set(ie_tup_valid))
+            print('Invalid tuples:\n\t{}'.format('\n'.join(invalid_tups)))
+        ie_tups += ie_tup_valid
+
     graph = nx.DiGraph()
 
     node_weights = defaultdict(int)
     alt_node_names = defaultdict(set)
     alt_edge_names = defaultdict(set)
-    for ie_output in ie_outputs:
-        for output in ie_output['verbs']:
-            verb = output['verb'].lower()
-            args = extract_args(output['description'])
-            if len(args) < 2:
-                continue
-            subject = args[0]
-            objects = args[1:]
 
-            resolved_subject = handle_node(subject, graph, alt_node_names, node_weights)
-            for obj in objects:
-                resolved_obj = handle_node(obj, graph, alt_node_names, node_weights)
-                if graph.has_edge(resolved_subject, resolved_obj):
-                    prev_edge_data = graph.get_edge_data(resolved_subject, resolved_obj)
-                    prev_weight, predicate = prev_edge_data['weight'], prev_edge_data['predicate']
-                    graph.remove_edge(resolved_subject, resolved_obj)
-                    graph.add_edge(resolved_subject, resolved_obj, weight=prev_weight + 1, predicate=predicate)
-                    edge_str = '{}|{}|{}'.format(resolved_subject, resolved_obj, predicate)
-                    alt_edge_names[edge_str].add(verb)
-                else:
-                    graph.add_edge(resolved_subject, resolved_obj, weight=1, predicate=verb)
+    for subject, verb, object in ie_tups:
+        resolved_subject = handle_node(subject, graph, alt_node_names, node_weights)
+        resolved_obj = handle_node(object, graph, alt_node_names, node_weights)
+        if graph.has_edge(resolved_subject, resolved_obj):
+            prev_edge_data = graph.get_edge_data(resolved_subject, resolved_obj)
+            prev_weight, predicate = prev_edge_data['weight'], prev_edge_data['predicate']
+            graph.remove_edge(resolved_subject, resolved_obj)
+            graph.add_edge(resolved_subject, resolved_obj, weight=prev_weight + 1, predicate=predicate)
+            edge_str = '{}|{}|{}'.format(resolved_subject, resolved_obj, predicate)
+            alt_edge_names[edge_str].add(verb)
+        else:
+            graph.add_edge(resolved_subject, resolved_obj, weight=1, predicate=verb)
 
     if verbose:
         pos = nx.spring_layout(graph)
@@ -124,11 +176,35 @@ def construct_graph(example, verbose=True):
     }
 
 
+def process_dataset(d, dtype, debug_mode=False):
+    debug_str = '_debug' if debug_mode else ''
+    out_fn = os.path.join('trivia_qa', 'graph_{}{}.pk'.format(dtype, debug_str))
+
+    graphs = list(map(lambda x: construct_graph(x, verbose=debug_mode), d))
+    qids = set(list(map(lambda c: c['question_id'], graphs)))
+    print('Unique QIDs={}'.format(len(qids)))
+    print(len(graphs))
+
+    with open(out_fn, 'w') as fd:
+        pickle.dump(graphs, fd)
+    print('Saved pickled output to {}'.format(out_fn))
+
+
 if __name__ == '__main__':
     debug_mode = len(sys.argv) > 1 and sys.argv[1] == 'debug'
 
     print('Loading Dataset')
-    dataset = load_dataset('trivia_qa', 'rc')
+    debug_data_fn = 'trivia_qa/train_mini.json'
+    if debug_mode and os.path.exists(debug_data_fn):
+        with open(debug_data_fn, 'r') as fd:
+            dataset = json.load(fd)
+    elif debug_mode and not os.path.exists(debug_data_fn):
+        dataset = load_dataset('trivia_qa', 'rc')['train']
+        dataset = [dict(e) for e in Subset(dataset, list(range(1, 11)))]
+        with open(debug_data_fn, 'w') as fd:
+            json.dump(dataset, fd)
+    else:
+        dataset = load_dataset('trivia_qa', 'rc')
     print('Loading Coref Pipeline')
     coref_nlp = spacy.load('en_core_web_lg')
     coref = neuralcoref.NeuralCoref(coref_nlp.vocab)
@@ -137,26 +213,23 @@ if __name__ == '__main__':
     sentence_nlp = spacy.load('en_core_web_lg')
     sentence_nlp.add_pipe(sentence_nlp.create_pipe('sentencizer'))
 
-    predictor = Predictor.from_path(
-        'https://storage.googleapis.com/allennlp-public-models/openie-model.2020.03.26.tar.gz')
+    archive = load_archive(
+        "../models/imojie",
+        weights_file="../models/imojie/model_state_epoch_7.th",
+        cuda_device=-1)
+
+    predictor = Predictor.from_archive(archive, "noie_seq2seq")
 
     with open('trivia_qa/tf_idf_vectorizer.pk', 'rb') as fd:
         tf_idf = pickle.load(fd)
 
-    dtypes = list(dataset.keys())  # train, test, etc,
-    for dtype in dtypes:
-        d = dataset[dtype]
-        if debug_mode:
-            d = Subset(d, list(range(1, 3)))
-        n = len(d)
-        print('Processing {} examples from {} set'.format(n, dtype))
-        out_fn = os.path.join('trivia_qa', 'graph_{}.pk'.format(dtype))
-
-        graphs = list(map(construct_graph, d))
-        qids = set(list(map(lambda c: c['question_id'], graphs)))
-        print('Unique QIDs={}'.format(len(qids)))
-        print(len(graphs))
-
-        with open(out_fn, 'w') as fd:
-            pickle.dump(graphs, fd)
-        print('Saved pickled output to {}'.format(out_fn))
+    if debug_mode:
+        print('Processing {} size sample train examples in debug mode'.format(len(dataset)))
+        process_dataset(dataset, 'train', debug_mode=True)
+    else:
+        dtypes = list(dataset.keys())  # train, test, etc
+        for dtype in dtypes:
+            d = dataset[dtype]
+            n = len(d)
+            print('Processing {} examples from {} set'.format(n, dtype))
+            process_dataset(d, dtype, debug_mode=True)
