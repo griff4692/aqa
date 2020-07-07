215432f08d9ba5a557aa77ff2d3414494cf52447
Griffin Adams <griff4692@gmail.com>
2020-07-06 21:08:36 -0400

    Custom graph.

--- data/main.py
+++ data/main.py
@@ -10,7 +10,6 @@ from allennlp.models.archival import load_archive
 from allennlp.common.util import import_submodules, sanitize
 import_submodules('imojie')
 import matplotlib.pyplot as plt
-import networkx as nx
 import neuralcoref
 from nlp import load_dataset
 import spacy
@@ -21,14 +20,73 @@ MODIFIER_REGEX = r'\[ARGM-[A-Z]+: [^\[\]]+)\]'
 SIM_THRESHOLD = 0.7
 
 
-def _node_match(new_node, graph, alt_names):
+class Node:
+    def __init__(self, name=None, weight=1):
+        self.name = name
+        self.weight = weight
+        self.alts = set()
+
+    def incr_weight(self):
+        self.weight += 1
+
+    def all_names(self):
+        if self.name in self.alts:
+            return self.alts
+        return [self.name] + list(self.alts)
+
+    def add_alt(self, alt_name):
+        self.alts.add(alt_name)
+
+
+class EdgeList:
+    def __init__(self, u):
+        self.u = u
+        self.v = []
+        self.edge_attrs = []
+
+    def add_edge(self, v, attrs):
+        self.v.append(v)
+        self.edge_attrs.append(attrs)
+
+    def incr_edge(self, edge_idx, predicate=None):
+        self.edge_attrs[edge_idx]['weight'] += 1
+        self.edge_attrs[edge_idx]['predicate'].add(predicate)
+
+    def idx(self, v):
+        for idx, node in enumerate(self.v):
+            if node.name == v.name:
+                return idx
+        return None
+
+
+class Graph:
+    def __init__(self):
+        self.adj_list = {}
+
+    def add_node(self, node_str):
+        assert node_str not in self.adj_list
+        u = Node(name=node_str, weight=1)
+        self.adj_list[node_str] = EdgeList(u)
+
+    def node_names(self):
+        return list(self.adj_list.keys())
+
+    def nodes(self):
+        return [e.u for e in self.adj_list.values()]
+
+    def add_edge(self, u, v, edge_attrs):
+        self.adj_list[u.name].add_edge(v, edge_attrs)
+
+    def edge_idx(self, u, v):
+        return self.adj_list[u].idx(v)
+
+
+def _node_match(new_node_str, graph):
     match = None
     max_sim = 0.0
     for node in graph.nodes():
-        full_comp = [node] + list(alt_names.get(new_node, []))
-        max_overlap = max(list(map(lambda x: overlap(new_node, x), full_comp)))
+        max_overlap = max(list(map(lambda x: overlap(new_node_str, x), node.all_names())))
         if max_overlap >= max(max_sim, SIM_THRESHOLD):
-            print('Merging \'{}\' --> \'{}\''.format(new_node, node))
             max_sim = max_overlap
             match = node
     return match
@@ -55,15 +113,15 @@ def extract_args(description):
     return results, modifiers
 
 
-def handle_node(node, graph, alt_names, node_weights):
-    match = _node_match(node, graph, alt_names)
-    resolved_node = match or node
+def resolve_node(new_node_str, graph) -> Node:
+    match = _node_match(new_node_str, graph)
     if match is None:
-        graph.add_node(node, weight=1)
+        match = graph.add_node(new_node_str)
     else:
-        alt_names[match].add(node)
-    node_weights[resolved_node] += 1
-    return resolved_node
+        print('Merging \'{}\' --> \'{}\''.format(new_node_str, match.name))
+        match.add_alt(new_node_str)
+        match.incr_weight()
+    return match
 
 
 def overlap(a, b):
@@ -72,7 +130,7 @@ def overlap(a, b):
 
 
 def process_imojie_output(token_ids):
-    temp=" ".join(token_ids)
+    temp = ' '.join(token_ids)
     temp = temp.replace(" ##","")
     temp = temp.replace("[unused1]","( ")
     temp = temp.replace("[unused2]"," ; ")
@@ -102,7 +160,6 @@ def _construct_ie_tuples(args, idx):
     for arg in args[idx]:
         for path in next_paths:
             results.append([arg] + path)
-
     return results
 
 
@@ -112,7 +169,7 @@ def construct_ie_tuples(args, verb):
     return all_paths_w_verb
 
 
-def construct_graph(example, verbose=True):
+def construct_graph(example):
     contexts = example['entity_pages']
     sents = []
     clusters = defaultdict(set)
@@ -140,52 +197,42 @@ def construct_graph(example, verbose=True):
             print('Invalid tuples:\n\t{}'.format('\n'.join(invalid_tups)))
         ie_tups += ie_tup_valid
 
-    graph = nx.DiGraph()
-
-    node_weights = defaultdict(int)
-    alt_node_names = defaultdict(set)
-    alt_edge_names = defaultdict(set)
-
+    graph = Graph()
     for subject, verb, object in ie_tups:
-        resolved_subject = handle_node(subject, graph, alt_node_names, node_weights)
-        resolved_obj = handle_node(object, graph, alt_node_names, node_weights)
-        if graph.has_edge(resolved_subject, resolved_obj):
-            prev_edge_data = graph.get_edge_data(resolved_subject, resolved_obj)
-            prev_weight, predicate = prev_edge_data['weight'], prev_edge_data['predicate']
-            graph.remove_edge(resolved_subject, resolved_obj)
-            graph.add_edge(resolved_subject, resolved_obj, weight=prev_weight + 1, predicate=predicate)
-            edge_str = '{}|{}|{}'.format(resolved_subject, resolved_obj, predicate)
-            alt_edge_names[edge_str].add(verb)
+        u = resolve_node(subject, graph)
+        v = resolve_node(object, graph)
+        edge_idx = graph.edge_idx(u, v)
+        if edge_idx is not None:
+            graph.adj_list[u.name].incr_edge(edge_idx, predicate=verb)
         else:
-            graph.add_edge(resolved_subject, resolved_obj, weight=1, predicate=verb)
+            attrs = {
+                'weight': 1,
+                'predicates': set([verb])
+            }
+            graph.add_edge(u, v, attrs)
 
-    if verbose:
-        pos = nx.spring_layout(graph)
-        nx.draw(graph, pos=pos)
-        nx.draw_networkx_labels(graph, pos=pos)
-        nx.draw_networkx_edge_labels(graph, pos=pos)
-        plt.show()
     # Include question_id just to make sure we serialize in same order (as FK into full dataset)
-    return {
+    obj_update = {
         'question_id': example['question_id'],
-        'alt_edge_names': alt_edge_names,
-        'alt_node_names': alt_node_names,
         'coref_contexts': coref_contexts,
         'clusters': clusters,
         'graph': graph,
     }
+
+    example.update(obj_update)
+    return example
 
 
 def process_dataset(d, dtype, debug_mode=False):
     debug_str = '_debug' if debug_mode else ''
     out_fn = os.path.join('trivia_qa', 'graph_{}{}.pk'.format(dtype, debug_str))
 
-    graphs = list(map(lambda x: construct_graph(x, verbose=debug_mode), d))
+    graphs = list(map(construct_graph, d))
     qids = set(list(map(lambda c: c['question_id'], graphs)))
     print('Unique QIDs={}'.format(len(qids)))
     print(len(graphs))
 
-    with open(out_fn, 'w') as fd:
+    with open(out_fn, 'wb') as fd:
         pickle.dump(graphs, fd)
     print('Saved pickled output to {}'.format(out_fn))
 
