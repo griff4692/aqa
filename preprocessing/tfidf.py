69745c0cce81102ab0005df0d6172a11b9ae0fc2
Mert Ketenci <mk4139@columbia.edu>
2020-07-10 15:47:01 +0300

    partial fix and speed test

--- preprocessing/tfidf.py
+++ preprocessing/tfidf.py
@@ -7,6 +7,7 @@ Created on Thu Jul  9 21:51:47 2020
 
 from collections import defaultdict
 from collections import Counter
+import json
 import time
 
 
@@ -18,14 +19,20 @@ import re
 import string
 from sklearn.feature_extraction.text import TfidfVectorizer
 import string
+from tqdm import tqdm
+
+
+def overlap(a, b, tf_idf_sk):
+    feats = tf_idf_sk.transform([a.lower(), b.lower()])
+    return (feats[0, :] * feats[1, :].T).todense()[0, 0]
 
 
 def clean_text(text, ignore_paranthesis = True):
     if text!=None:
         if ignore_paranthesis:
             text = re.sub("[\(\[].*?[\)\]]", "", text)
-            
         text = text.translate(str.maketrans('', '', string.punctuation))
+        text = ' '.join(text.split())
         return text.lower()
     
 
@@ -35,17 +42,17 @@ class tf_idf:
         self.idf = defaultdict(int)
         
     def fit(self, documents):
-        num_documents = len(documents)
+        self.log_num_documents = np.log(len(documents) + 1)
         for document in documents:
             document = set(document.split(' '))
             for word in document:
                 self.idf[word] += 1
         for key in self.idf.keys():
-            self.idf[key] = num_documents / self.idf[key]
-
+            self.idf[key] = self.idf[key]
+        
     def similarity(self, x, y):
-        x = x.split(' ')
-        y = y.split(' ')
+        x = x.lower().split(' ')
+        y = y.lower().split(' ')
         
         set_x = set(x)
         set_y = set(y)
@@ -68,63 +75,63 @@ class tf_idf:
         tf_idf_x = self.calc_tf_idf(tf_x, idf_x)
         tf_idf_y = self.calc_tf_idf(tf_y, idf_y)
         
-        return self.cosine_sim(tf_idf_x_intersection, 
-                               tf_idf_y_intersection, tf_idf_x, tf_idf_y)
-        
-    def calc_tf(self, x, intersection):
+        tf_idf_x_intersection /= np.sqrt(np.dot(tf_idf_x, tf_idf_x))
+        tf_idf_y_intersection /= np.sqrt(np.dot(tf_idf_y, tf_idf_y))
+        return self.cosine_sim(tf_idf_x_intersection, tf_idf_y_intersection)
+                                
+    def calc_tf(self, x, unique):
         tf = Counter(x)
-        return np.asarray([tf[word] for word in intersection]) / len(x)
+        return np.asarray([tf[word] for word in unique]) / len(x)
     
-    def calc_idf(self, intersection):        
-        return np.log([max(1, self.idf[word]) for word in intersection])
+    def calc_idf(self, words):        
+        return self.log_num_documents - np.log([self.idf[word] + 1 for word in words]) + 1
     
     def calc_tf_idf(self, tf, idf):
         return tf * idf
     
-    def cosine_sim(self, tf_idf_x_intersection, tf_idf_y_intersection, tf_idf_x, tf_idf_y):
+    def cosine_sim(self, tf_idf_x_intersection, tf_idf_y_intersection):
         cosine = np.dot(tf_idf_x_intersection, tf_idf_y_intersection)
-        mag_x = np.dot(tf_idf_x, tf_idf_x)
-        mag_y = np.dot(tf_idf_y, tf_idf_y)
-        return cosine/(np.sqrt(mag_x * mag_y) + 1e-40)
+        return cosine
    
-
-print('Loading dataset...')
-input_fn = './trivia_qa/train_mini.json'
-with open(input_fn) as f:
-    dataset = json.load(f)
-
-token_corpus = []
-n = len(dataset)
-print('Adding corpus documents.')
-for i, example in enumerate(dataset):
-    token_corpus += example['entity_pages']['wiki_context']
-    if (i + 1) % 10000 == 0:
-        print('Added wiki docs from {} out of {} examples'.format(i + 1, n))
+if __name__ == '__main__':
+    
+    print('Loading dataset...')
+    input_fn = './trivia_qa/train_mini.json'
+    with open(input_fn) as f:
+        dataset = json.load(f)
+    
+    token_corpus = []
+    n = len(dataset)
+    print('Adding corpus documents.')
+    for i, example in enumerate(dataset):
+        token_corpus += example['entity_pages']['wiki_context']
+        if (i + 1) % 10000 == 0:
+            print('Added wiki docs from {} out of {} examples'.format(i + 1, n))
+            
+    documents = [clean_text(text) for text in token_corpus]
+    
+    a = ['see me walking down fifth avenue', 'a walking cane here at my side']
+    
+    print('Fitting TF-IDF vectorizer')
+    tfidf = tf_idf()
+    tfidf.fit(documents)
+    
+    vectorizer = TfidfVectorizer()
+    tf_idf_sk = vectorizer.fit(documents)
+    
+    ourtf = []
+    theirtf = []
+    
+    for i in tqdm(range(100000)):
         
-token_corpus = [clean_text(text) for text in token_corpus]
-
-print('Fitting TF-IDF vectorizer')
-tfidf = tf_idf()
-tfidf.fit(token_corpus)
-
-vectorizer = TfidfVectorizer()
-tf_idf_sk = vectorizer.fit(token_corpus)
-
-a = ['See me walking down Fifth Avenue', 'A walking cane here at my side']
-a  = [x.lower() for x in a]
- 
-t0 = time.time()
-print(tfidf.similarity(a[0], a[1]))
-t1 = time.time()
-print(t1-t0)
-
-
-def overlap(a, b, tf_idf_sk):
-    feats = tf_idf_sk.transform([a.lower(), b.lower()])
-    return (feats[0, :] * feats[1, :].T).todense()[0, 0]
-
-
-t0 = time.time()
-print(overlap(a[0], a[1], tf_idf_sk))
-t1 = time.time()
-print(t1-t0)
+        t0 = time.time()
+        tfidf.similarity(a[0], a[1])
+        t1 = time.time()
+        ourtf.append(t1-t0)
+        
+        t0 = time.time()
+        overlap(a[0], a[1], tf_idf_sk)
+        t1 = time.time()
+        theirtf.append(t1-t0)
+    
+    print('Our tfidf is {} times faster'.format(np.mean(theirtf) / np.mean(ourtf)))
