7a3c6a016ed3f161cd8703817a81dc90159d5213
Griffin Adams <griff4692@gmail.com>
2020-07-11 13:41:23 -0400

    Batch coref.

--- setup.sh
+++ setup.sh
@@ -2,4 +2,4 @@ conda create --name aqa python=3.7
 conda activate aqa
 conda install pytorch torchvision cudatoolkit=10.2 -c pytorch
 python install -r requirements.txt
-
+python -m spacy download en_core_web_lg
